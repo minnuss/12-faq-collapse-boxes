ed210544015651b5241a90dc38f4b7a60a31eee4
minnuss <ponjevic.goran@hotmail.com>
2021-04-04 18:35:10 +0200

    simpler js code

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-const btnOpen = document.querySelectorAll('.fa-chevron-down');
-const btnClose = document.querySelectorAll('.fa-times-circle');
+// const btnOpen = document.querySelectorAll('.fa-chevron-down');
+// const btnClose = document.querySelectorAll('.fa-times-circle');
 
 
-btnOpen.forEach(open => {
-    open.addEventListener('click', () => {
-        open.parentElement.classList.add('active');
+// btnOpen.forEach(open => {
+//     open.addEventListener('click', () => {
+//         open.parentElement.classList.add('active');
+//     })
+// });
+
+// btnClose.forEach(open => {
+//     open.addEventListener('click', () => {
+//         open.parentElement.classList.remove('active');
+//     })
+// });
+
+
+// SIMPLE CODE THEN PREVIOUS
+const toggle = document.querySelectorAll('.faq__content__box i');
+
+toggle.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.parentElement.classList.toggle('active');
     })
-});
-
-btnClose.forEach(open => {
-    open.addEventListener('click', () => {
-        open.parentElement.classList.remove('active');
-    })
-});
-
+})
